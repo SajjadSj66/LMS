e3d9bd24623119850599e954d0f8629dbf9081d5
SajjadSj66 <147406621+SajjadSj66@users.noreply.github.com>
2025-02-24 14:14:39 +0330

    Delete LMS directory

--- LMS/LMS/__init__.py
+++
@@ -1,6 +0,0 @@
-from __future__ import absolute_import, unicode_literals
-
-# این تضمین می‌کند که Celery هنگام شروع پروژه load شود
-from .celery import app as celery_app
-
-__all__ = ('celery_app',)
